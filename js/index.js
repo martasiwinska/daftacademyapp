ecdafcf68b1424f10503e4a9fba2cff87ea1d9fa
Marta Siwińska <mar.siwinska@gmail.com>
2019-03-26 21:58:17 +0100

    webpack app

---
+++ js/index.js
@@ -0,0 +1,3 @@
+import {default as Loffki} from './script';
+
+Loffki();
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './js/index.js',
+    output: {
+        filename: 'index_bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin()],
+};
